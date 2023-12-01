1f3489a21102698511e154e594fb3d6ebb1ba787
Evan Jackson <evanojackson@gmail.com>
2023-11-30 22:38:55 -0600

    updated logic

--- connect.py
+++ connect.py
@@ -1,31 +1,20 @@
 import smtplib
 import credentials
 
-def sendEmails(pairs: dict, emails: dict):
+def sendEmails(pairs: list, emails: dict):
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
     s.login(credentials.FROM, credentials.appPassword)
-    for key in pairs:
-        value = pairs[key]
-        message = f'''Subject: Secret Santa 2022\n\nWelcome to the Homies with X Chromies 2nd annual secret santa! Thank you for participating {key}! Before I tell you who you are buying for, let me remind you of the rules.
-        1) $20 spending limit
+    for Santa, Secret in pairs:
+        message = f'''Subject: Secret Santa 2023\n\nWelcome to the Homies with X Chromies 3rd annual secret santa! Thank you for participating {Santa}! Before I tell you who you are buying for, let me remind you of the rules.
+        1) $25 spending limit
         2) You may not tell anyone else who you are buying for
-        3) Gifts will all be exchanged on a TBD date. Check group chat for updates
+        3) This year, you may need to mail it to the recipient, so their address will be included if that is needed.
         4) You cannot buy for your s/o. This should already be implemented but if you recieve your s/o please let the groupchat know so that can be addressed.
         
-Now for the big reveal! You will be purchasing for {value}! Their interests include {emails[value]["interests"]}.
+Now for the big reveal! You will be purchasing for {Secret}! Their interests include {emails[Secret]["interests"]}. Their address is {emails[Secret]["address"]}.
         
 Have fun and see you soon!'''
-        target = emails[key]["email"]
-        s.sendmail(credentials.FROM, emails[key]["email"], message)
+        target = emails[Santa]["email"]
+        s.sendmail(credentials.FROM, target, message)
     s.quit()
-
-
-illegal = {
-    "Dylan":"Rachel",
-    "Rachel":"Dylan",
-    "Chris":"Sarah",
-    "Sarah":"Chris",
-    "Ron":"Sydney",
-    "Sydney":"Ron"
-}
--- main.py
+++ main.py
@@ -1,42 +1,74 @@
 import csv
 import random
+from pprint import pprint
 from datetime import datetime
-from connect import illegal, sendEmails
-random.seed(datetime.now())
+from connect import sendEmails
+
+
+def create_secret_santa_list(names, excluded_pairings={}):
+    random.seed(datetime.now().timestamp())
+    secret_santa_list = []
+    remaining_names = names.copy()
+
+    for giver in names:
+        possible_recipients = [name for name in remaining_names if name != giver and name not in excluded_pairings.get(giver, [])]
+        
+        if not possible_recipients:
+            # If there are no valid recipients, start over
+            return create_secret_santa_list(names, excluded_pairings)
+
+        recipient = random.choice(possible_recipients)
+        secret_santa_list.append((giver, recipient))
+        remaining_names.remove(recipient)
+
+    return secret_santa_list
+
+def process_csv(csv_file_path):
+    data_dict = {}
+    names = []
+
+    with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        
+        for row in reader:
+            name = row['Name'].strip()
+            email = row['Email'].strip()
+            interests = row['Interests'].strip()
+            address = row['Address'].strip()
+
+            names.append(name)
+
+            # Create the inner dictionary
+            inner_dict = {'email': email, 'interests': interests, 'address': address}
+
+            # Add to the main dictionary
+            data_dict[name] = inner_dict
+
+    return names, data_dict
+
 #dictionary of illegal matchups
+illegal = {
+    "Dylan":["Rachel"],
+    "Rachel":["Dylan"],
+    "Chris":["Sarah", "Evan"],
+    "Sarah":["Chris"],
+    "Evan":["Chris"],
+    "Ron":["Sydney"],
+    "Sydney":["Ron"],
+    "Keaton":["Ally"], 
+    "Ally":["Keaton"]
+}
 
 #fill dictionary with name email pair from csv file. csv file is formatted as Name,Email
 emails = {}
 pairs = {}
 #create 2 lists so that I can make pairs
-l1 = []
-l2 = []
+names = []
 complete = False
 
-with open("emails.csv", newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in reader:
-        emails[row[0]] = {"email":row[1], "interests": ','.join(row[2:])}
-        l1.append(row[0])
-        l2.append(row[0])
+names, emails = process_csv('emails.csv')
 
-#remove the header from each list
-del(emails["Name"])
-l1.remove("Name")
-l2.remove("Name")
+pairs = create_secret_santa_list(names, illegal)
 
-while(not complete):
-    #assume it works
-    complete = True
-    #shuffle both lists
-    random.shuffle(l1)
-    random.shuffle(l2)
-    for i in range(len(l1)):
-        pairs[l1[i]] = l2[i]
-    
-    for key in pairs:
-        #try again if anyone matched with themselves, their s/o, or each other
-        if key == pairs[key] or (key in illegal and illegal[key] == pairs[key]) or key == pairs[pairs[key]] :
-            complete = False
 sendEmails(pairs, emails)
 
